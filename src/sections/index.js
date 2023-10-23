6223d2ef66382c16dd8a1f8d8eaf489917117845
Deepak <dk5007667@gmail.com>
2023-10-23 11:41:39 +0530

    intial commit

---
+++ src/sections/index.js
@@ -0,0 +1,19 @@
+import Hero from './Hero';
+import PopularProducts from './PopularProducts';
+import Services from './Services';
+import SpecialOffers from './SpecialOffers';
+import CustomerReviews from './CustomerReviews';
+import Footer from './Footer';
+import Subscribe from './Subscribe';
+import SuperQuality from './SuperQuality';
+
+export{
+    Hero,
+    Subscribe,
+    PopularProducts,
+    SuperQuality,
+    SpecialOffers,
+    CustomerReviews,
+    Services,
+    Footer
+}
